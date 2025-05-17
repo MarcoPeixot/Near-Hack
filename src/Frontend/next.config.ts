0381cfac298960fd073da414456ddf779b65a53b
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2025-05-17 16:50:28 -0300

    feat: school page

--- src/Frontend/next.config.ts
+++ src/Frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/lumx/:path*',
+        destination: `${process.env.NEXT_PUBLIC_LUMX_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
